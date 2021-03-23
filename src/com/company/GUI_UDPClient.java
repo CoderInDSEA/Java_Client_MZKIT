92e0c883c37a5e7bc046f89d107fa4ee6ae7b15f
Yaroslav <55196529+YaroslavKotov-Program@users.noreply.github.com>
2021-03-23 11:02:03 +0200

    GUI

---
+++ src/com/company/GUI_UDPClient.java
@@ -0,0 +1,88 @@
+package com.company;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.util.Scanner;
+
+public class GUI_UDPClient extends Frame implements ActionListener {
+    private Label LabelGUI;    // Declare a Label component
+    public TextField TextFieldGui; // Declare a TextField component
+    public Button ButtonGui;   // Declare a Button component
+    public String command ;
+    public final static int SERVICE_PORT = 50001;
+
+    // Constructor to setup GUI components and event handlers
+    public GUI_UDPClient() {
+        setLayout(new FlowLayout());
+        // "super" Frame, which is a Container, sets its layout to FlowLayout to arrange
+        // the components from left-to-right, and flow to next row from top-to-bottom.
+
+        LabelGUI = new Label("Command Enter");  // construct the Label component
+        add(LabelGUI);                    // "super" Frame container adds Label component
+
+        TextFieldGui = new TextField( 20); // construct the TextField component with initial text
+        TextFieldGui.setEditable(true);       // set to read-only
+        add(TextFieldGui);                     // "super" Frame container adds TextField component
+
+        ButtonGui = new Button("Send");   // construct the Button component
+        add(ButtonGui);                    // "super" Frame container adds Button component
+
+        ButtonGui.addActionListener(this);
+
+
+        setTitle("AWT Counter");  // "super" Frame sets its title
+        setSize(250, 100);        // "super" Frame sets its initial window size
+        setVisible(true);         // "super" Frame shows
+    }
+
+    // ActionEvent handler - Called back upon button-click.
+    @Override
+    public void actionPerformed(ActionEvent evt)   {
+        try {
+            command = TextFieldGui.getText();
+            ClientFunc(command);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public String ClientFunc(String command) throws IOException{
+        try {
+            DatagramSocket clientSocket = new DatagramSocket();
+
+            // Получите IP-адрес сервера
+            InetAddress IPAddress = InetAddress.getByName("localhost");
+
+            Scanner input = new Scanner(System.in);
+            //String command ;
+            //System.out.println("Enter command : " );
+            //command = input.nextLine();
+            byte[] sendingDataBuffer = new byte[command.length()];
+            byte[] sendingBufferCommandInit = new byte[1024];
+            sendingDataBuffer = command.getBytes();
+            int indexPar = command.indexOf ("%") ;
+
+
+            DatagramPacket sendingPacket = new DatagramPacket(sendingDataBuffer,sendingDataBuffer.length,IPAddress, SERVICE_PORT);
+            clientSocket.send(sendingPacket);
+            byte[] receivingDataBuffer = new byte[sendingDataBuffer.length];
+            DatagramPacket receivingPacket = new DatagramPacket(receivingDataBuffer,receivingDataBuffer.length);
+            clientSocket.receive(receivingPacket);
+            String receivedData = new String(receivingPacket.getData());
+            System.out.println("Sent from the server: "+receivedData);
+
+            // Закройте соединение с сервером через сокет
+            clientSocket.close();
+        }
+        catch(SocketException e) {
+            e.printStackTrace();
+        }
+        return "Fail";
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,44 +1,14 @@
 package com.company;
 
+
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.SocketException;
-import java.util.Scanner;
 
 public class Main{
-    public final static int SERVICE_PORT = 50001;
+    public static void main(String[] args) throws IOException {
+        //UDP_ClientClass ClientClass = new UDP_ClientClass();
+        //ClientClass.UDP_ClientFunc();
+        GUI_UDPClient guiClient = new GUI_UDPClient() ;
 
-    public static void main(String[] args) throws IOException{
-        try{
 
-            DatagramSocket clientSocket = new DatagramSocket();
-
-            // Получите IP-адрес сервера
-            InetAddress IPAddress = InetAddress.getByName("localhost");
-
-            byte[] sendingDataBuffer = new byte[1024];
-            byte[] receivingDataBuffer = new byte[1024];
-
-            Scanner input = new Scanner(System.in);
-            String command ;
-            System.out.println("Enter command : " );
-            command = input.nextLine();
-            sendingDataBuffer = command.getBytes();
-
-            DatagramPacket sendingPacket = new DatagramPacket(sendingDataBuffer,sendingDataBuffer.length,IPAddress, SERVICE_PORT);
-            clientSocket.send(sendingPacket);
-            DatagramPacket receivingPacket = new DatagramPacket(receivingDataBuffer,receivingDataBuffer.length);
-            clientSocket.receive(receivingPacket);
-            String receivedData = new String(receivingPacket.getData());
-            System.out.println("Sent from the server: "+receivedData);
-
-            // Закройте соединение с сервером через сокет
-            clientSocket.close();
-        }
-        catch(SocketException e) {
-            e.printStackTrace();
-        }
     }
 }
